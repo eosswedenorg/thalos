cd8eae6331c7a120e1bd6f7267ca13f5f1670754
Henrik Hautakoski <henrik@eossweden.org>
2022-01-17 15:33:03 +0100

    types.go: Add json tags

--- types.go
+++ types.go
@@ -6,8 +6,8 @@ import (
 )
 
 type ActionTrace struct {
-    Receiver   eos.Name
-    Contract   eos.AccountName
-	Action     eos.ActionName
-	Data       interface{}
+    Receiver   eos.Name `json:"receiver"`
+    Contract   eos.AccountName `json:"contract"`
+	Action     eos.ActionName `json:"action"`
+	Data       interface{} `json:"data"`
 }
