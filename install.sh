93c9f8a721f215615f8d9e443fd68212ae8966ce
Henrik Hautakoski <henrik@eossweden.org>
2022-01-20 11:23:42 +0100

    install.sh: only copy config.example.json if $INSTALL_DIR/config.json does not exist.

--- install.sh
+++ install.sh
@@ -9,16 +9,13 @@ INSTALL_DIR=$1
 
 echo "Installing application in: $INSTALL_DIR"
 
-if [ -f "config.json" ];then
-    CONFIG_FILE=./config.json
-else :
-    CONFIG_FILE=config.example.json
-fi
-
 mkdir -p "$INSTALL_DIR"/{bin,logs}
 install -m 750 -t "${INSTALL_DIR}/bin" build/eosio-ship-trace-reader
 
-install -T -m 600 ${CONFIG_FILE} "${INSTALL_DIR}/config.json"
+if [ ! -f "${INSTALL_DIR}/config.json" ]; then
+    install -T -m 600 config.example.json "${INSTALL_DIR}/config.json"
+fi
+
 install -m 750 -t "${INSTALL_DIR}" scripts/start.sh scripts/stop.sh
 
 echo "Done"
