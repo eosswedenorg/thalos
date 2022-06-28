fb0ecce6c2b54a13a3762a348a461a7147a3cf72
Henrik Hautakoski <henrik@eossweden.org>
2022-06-28 14:02:52 +0200

    redis/wrapper.go: Adding XAdd()

--- redis/wrapper.go
+++ redis/wrapper.go
@@ -48,6 +48,18 @@ func Publish(channel string, message interface{}) (*_redis.IntCmd) {
     return rdb.Publish(redisCtx, channel, message)
 }
 
+func XAdd(stream string, id string, maxlen int64, message map[string]interface{}) (*_redis.StringCmd) {
+
+    args := &_redis.XAddArgs{
+    	Stream: stream,
+    	ID: id,
+        MaxLenApprox: maxlen,
+    	Values: message,
+    }
+
+    return rdb.XAdd(redisCtx, args)
+}
+
 func RegisterPublish(channel string, message interface{}) (*_redis.IntCmd) {
     return redis_pipe.Publish(redisCtx, channel, message)
 }
