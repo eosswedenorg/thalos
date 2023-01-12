caf74608758ea40888de2aa6d55e05d8f2ceecc4
Henrik Hautakoski <henrik@eossweden.org>
2023-01-12 15:03:31 +0100

    internal/redis/channel.go: Don't need to use fmt.Sprintf() anymore.

--- internal/redis/channel.go
+++ internal/redis/channel.go
@@ -1,7 +1,6 @@
 package redis
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -38,11 +37,11 @@ func (ac ActionChannel) String() string {
 	ch := Channel{"actions"}
 
 	if len(ac.Contract) > 0 {
-		ch.Append(fmt.Sprintf("contract:%s", ac.Contract))
+		ch.Append("contract", ac.Contract)
 	}
 
 	if len(ac.Action) > 0 {
-		ch.Append(fmt.Sprintf("action:%s", ac.Action))
+		ch.Append("action", ac.Action)
 	}
 
 	return ch.String()
