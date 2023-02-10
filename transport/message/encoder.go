a20295ee88a34a8b2351f8437424343040b4dda3
Henrik Hautakoski <henrik@eossweden.org>
2023-02-10 16:50:59 +0100

    transport/message/encoder.go: remove Encode() function.

--- transport/message/encoder.go
+++ transport/message/encoder.go
@@ -1,21 +1,4 @@
 package message
 
-import (
-	"encoding/json"
-
-	log "github.com/sirupsen/logrus"
-)
-
 // Encoder is a function that can encode a object to the encoded format.
 type Encoder func(v any) ([]byte, error)
-
-func Encode(v interface{}) ([]byte, bool) {
-	payload, err := json.Marshal(v)
-	if err != nil {
-		log.WithError(err).
-			WithField("v", v).
-			Warn("Failed to encode message to json")
-		return nil, false
-	}
-	return payload, true
-}
