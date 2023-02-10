315d7e19c965b2b1322be22c4b7348f36b1971db
Henrik Hautakoski <henrik@eossweden.org>
2023-02-10 16:54:31 +0100

    transport/message/encoding.go: Add Decoder and Codec

--- transport/message/encoding.go
+++ transport/message/encoding.go
@@ -2,3 +2,12 @@ package message
 
 // Encoder is a function that can encode a object to the encoded format.
 type Encoder func(any) ([]byte, error)
+
+// Decoder is a function that can decode a format into an object
+type Decoder func(any, []byte) error
+
+// Codec is a type that can has a matching Encoder and Decoder function.
+type Codec struct {
+	Encoder Encoder
+	Decoder Decoder
+}
