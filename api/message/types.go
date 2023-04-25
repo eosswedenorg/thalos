f3900bc2621784d904d8e82e2a777eb15742dc90
Henrik Hautakoski <henrik@eossweden.org>
2023-04-25 16:53:46 +0200

    api/message/types.go: add msgpack tags

--- api/message/types.go
+++ api/message/types.go
@@ -1,21 +1,21 @@
 package message
 
 type HeartBeat struct {
-	BlockNum                 uint32 `json:"blocknum"`
-	HeadBlockNum             uint32 `json:"head_blocknum"`
-	LastIrreversibleBlockNum uint32 `json:"last_irreversible_blocknum"`
+	BlockNum                 uint32 `json:"blocknum" msgpack:"blocknum"`
+	HeadBlockNum             uint32 `json:"head_blocknum" msgpack:"head_blocknum"`
+	LastIrreversibleBlockNum uint32 `json:"last_irreversible_blocknum" msgpack:"last_irreversible_blocknum"`
 }
 
 type ActionTrace struct {
-	TxID string `json:"tx_id"`
+	TxID string `json:"tx_id" msgpack:"tx_id"`
 
 	// Action name
-	Name string `json:"name"`
+	Name string `json:"name" msgpack:"name"`
 
 	// Contract account.
-	Contract string `json:"contract"`
+	Contract string `json:"contract" msgpack:"contract"`
 
-	Receiver string      `json:"receiver"`
-	Data     interface{} `json:"data"`
-	HexData  string      `json:"hex_data"`
+	Receiver string      `json:"receiver" msgpack:"receiver"`
+	Data     interface{} `json:"data" msgpack:"data"`
+	HexData  string      `json:"hex_data" msgpack:"hex_data"`
 }
