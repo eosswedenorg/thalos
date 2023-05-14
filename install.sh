30f3ff518756c1fbbf4bb57b1be355e5fdc1477b
Henrik Hautakoski <henrik@eossweden.org>
2023-05-14 18:09:39 +0200

    install.sh: Use Makefile to install stuff.

--- install.sh
+++ install.sh
@@ -10,12 +10,7 @@ INSTALL_DIR=$1
 echo "Installing application in: $INSTALL_DIR"
 
 mkdir -p "$INSTALL_DIR"/{bin,logs}
-install -m 750 -t "${INSTALL_DIR}/bin" build/thalos-server
 
-if [ ! -f "${INSTALL_DIR}/config.yml" ]; then
-    install -T -m 600 config.example.yml "${INSTALL_DIR}/config.yml"
-fi
-
-install -m 750 -t "${INSTALL_DIR}" scripts/start.sh scripts/stop.sh
+make -e DESTDIR=$INSTALL_DIR PREFIX= CFGDIR= install install-scripts
 
 echo "Done"
