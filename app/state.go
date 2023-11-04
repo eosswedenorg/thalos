1ae3da425c2e0f0e5b32db7d10a5510a3a2a652f
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-11-04 13:21:30 +0100

    app/state.go: Adding StateLoader and StateSaver function types.

--- app/state.go
+++ app/state.go
@@ -5,3 +5,11 @@ type State struct {
 	// Last processed block
 	CurrentBlock uint32
 }
+
+type (
+	// StateLoader is a function that loads a state.
+	StateLoader func(*State)
+
+	// StateSaver is a function that saves a state.
+	StateSaver func(State) error
+)
