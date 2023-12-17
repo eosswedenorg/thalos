8b3202e4d377c9148b914aace44ce305c367ca2a
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-12-17 17:51:32 +0100

    app/cache/store.go: make interface context aware.

--- app/cache/store.go
+++ app/cache/store.go
@@ -1,15 +1,18 @@
 package cache
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Store interface {
 	// Set an item in the store.
-	Set(key string, value any, TTL time.Duration) error
+	Set(ctx context.Context, key string, value any, TTL time.Duration) error
 
 	// Get an item from the store.
 	// returns an error if key is not found or there is other problems.
-	Get(key string, value any) error
+	Get(ctx context.Context, key string, value any) error
 
 	// Check if a key exist in the store.
-	Has(key string) bool
+	Has(ctx context.Context, key string) bool
 }
