daa89cf3728bf56d660ff41f4d168337f3204479
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-02-07 17:30:19 +0100

    api/reader.go: Document io.EOF

--- api/reader.go
+++ api/reader.go
@@ -8,10 +8,13 @@ type Reader interface {
 	// Read a message from a channel.
 	// Read may block until a message is ready or an error occured.
 	//
+	// io.EOF is returned from a reader when there is no more data to be read.
+	// If Read returns io.EOF all subsequent calls must also return io.EOF
+	//
 	// This function should be designed to handle concurrent calls. eg. thread safe.
 	Read(channel Channel) ([]byte, error)
 
 	// Close closes the reader
-	// Any blocked Read operations will be unblocked.
+	// Any blocked Read operations will be unblocked and return io.EOF
 	Close() error
 }
