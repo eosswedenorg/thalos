b517943fee88ad373407b336168223823808ca1f
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-02-17 16:31:14 +0100

    app/log/config.go: add mapstructure tags

--- app/log/config.go
+++ app/log/config.go
@@ -10,16 +10,16 @@ import (
 // Config represents configuration parameters for a log.
 type Config struct {
 	// Filename where the log is stored.
-	Filename string `yaml:"filename"`
+	Filename string `yaml:"filename" mapstructure:"filename"`
 
 	// Directory where the log files are stored.
-	Directory string `yaml:"directory"`
+	Directory string `yaml:"directory" mapstructure:"directory"`
 
 	// Maximum filesize, the log is rotated when this size is exceeded.
-	MaxFileSize types.Size `yaml:"maxfilesize"`
+	MaxFileSize types.Size `yaml:"maxfilesize" mapstructure:"maxfilesize"`
 
 	// Maximum lifetime of the file before it is rotated.
-	MaxTime time.Duration `yaml:"maxtime"`
+	MaxTime time.Duration `yaml:"maxtime" mapstructure:"maxtime"`
 }
 
 func (c Config) GetFilename() string {
