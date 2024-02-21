d6a7bcd2bbcde83b857a27a21031a04acc0d869b
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-02-21 17:29:31 +0100

    internal/log/RotatingFileOptions.go: add WithTimestampFormat function

--- internal/log/RotatingFileOptions.go
+++ internal/log/RotatingFileOptions.go
@@ -4,6 +4,14 @@ import "time"
 
 type RotatingFileOption func(*RotatingFile)
 
+func WithTimestampFormat(value string) RotatingFileOption {
+	return func(f *RotatingFile) {
+		if len(value) > 0 {
+			f.format = value
+		}
+	}
+}
+
 func WithMaxSize(value int64) RotatingFileOption {
 	return func(f *RotatingFile) {
 		f.maxSize = value
