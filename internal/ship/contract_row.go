6d7d004b2b16b41c8c5880a8bada2a9d349931c9
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-08-11 17:03:42 +0200

    internal/ship/contract_row.go: adding DecodeContractRow that actually decodes data from abi

--- internal/ship/contract_row.go
+++ internal/ship/contract_row.go
@@ -1,6 +1,9 @@
 package ship
 
 import (
+	"bytes"
+
+	"github.com/eosswedenorg/thalos/internal/abi"
 	"github.com/mitchellh/mapstructure"
 	"github.com/shufflingpixels/antelope-go/chain"
 )
@@ -18,4 +21,17 @@ func ParseContractRow(v map[string]interface{}) (*ContractRow, error) {
 	out := &ContractRow{}
 	err := mapstructure.WeakDecode(v, out)
 	return out, err
+}
+
+func DecodeContractRow(manager *abi.AbiManager, data map[string]any) (any, error) {
+	row, err := ParseContractRow(data)
+	if err != nil {
+		return nil, err
+	}
+
+	abi, err := manager.GetAbi(row.Code)
+	if err != nil {
+		return nil, err
+	}
+	return abi.DecodeTable(bytes.NewReader(row.Value), row.Table)
 }
